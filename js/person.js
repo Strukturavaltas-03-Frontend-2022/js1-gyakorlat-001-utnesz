dba15cf135cf51ffc77ca382595c8a273cb89f02
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-10 22:07:35 +0200

    Ready

---
+++ js/person.js
@@ -0,0 +1,3 @@
+const personDataLog = 
+(firstName = 'John', lastName = 'Doe', age = '33') => 'My name is ' + firstName + " " + lastName + ". I'm " + age + ' years old.';
+console.log(personDataLog('Mike', 'Stevenson', '42'));
